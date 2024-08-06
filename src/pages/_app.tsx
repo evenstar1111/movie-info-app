e530c140759463c84b7578aaf6b7fcc81ff0c1f4
Sk <sahil@pitangent.com>
2024-08-06 10:17:15 +0530

    feat: export custom mui theme from theme folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,10 @@
+import theme from '@/theme/theme';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProps } from 'next/app';
-import { Roboto } from 'next/font/google';
 import '../styles/globals.scss';
 import '../styles/no_gutter.scss';
-
-const roboto = Roboto({
-   weight: ['300', '400', '500', '700'],
-   subsets: ['latin'],
-   display: 'swap',
-});
-
-const theme = createTheme({
-   typography: {
-      fontFamily: roboto.style.fontFamily,
-   },
-});
 
 export default function App(props: AppProps) {
    const { Component, pageProps } = props;
