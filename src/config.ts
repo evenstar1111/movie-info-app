08dcf457804105c61dee74f6dbc51e485490fb0d
Sk <sahil@pitangent.com>
2024-08-08 11:05:32 +0530

    refactor: convert config.js to config.ts; store tmdb configs in an object

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,12 @@ import getConfig from 'next/config';
 
 const { serverRuntimeConfig } = getConfig();
 
-export const KEY = process.env.KEY || serverRuntimeConfig.KEY;
 export const image_base = 'https://image.tmdb.org/t/p/w200';
 export const image_base_lg = 'https://image.tmdb.org/t/p/w300';
 export const imdb_link_tmov = 'https://www.imdb.com/title/';
 export const imdb_link_psn = 'https://www.imdb.com/name/';
+
+export const tmdbConfig = {
+   apiKey: process.env.TMDB_API_KEY || serverRuntimeConfig.TMDB_API_KEY,
+   baseUrl: process.env.TMDB_API_BASE_URL,
+} as const;
